e5c0be6db14ae21e73a273680753ea8bedc3d019
Manjunath NM <manjunath.nm89@gmail.com>
2011-05-27 18:28:47 +0530

    :

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+ group :development do
+   gem 'annotate-models','1.0.4'
+ end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SignupApp::Application.config.secret_token = 'c644542f4bbd1a71a2b3a39c49be1fd6091e79532dabd46cdc7a665be5e18ed86c4b2c038551b8cf0787f3ce8fee24c7300da4945dd0efb2b0fdccb8acd48259'
