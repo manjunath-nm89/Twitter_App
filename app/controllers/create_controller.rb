8f7d41866a5d4177708810f7532fb44103714e55
Manjunath <manjunath.nm89@gmail.com>
2011-06-02 15:42:04 +0530

    Finished Signup Form

--- app/controllers/create_controller.rb
+++ app/controllers/create_controller.rb
@@ -6,6 +6,17 @@ class CreateController < ApplicationController
   end
   
   def users
+    @title="Signup"
+    @user=User.new(params[:user])
+    if @user.save
+      redirect_to "welcome" ,@user
+    else
+      render "signup"
+    end
+  end
+
+  def welcome
+    @title="Welcome"
     
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SignupApp::Application.routes.draw do
+
+  resources :users
+  
   root :to => "create#signup"
   
   match "/users",:to=>"create#users"
-  
+  match "/welcome",:to=>"create#welcome"
 end
