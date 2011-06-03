b3bdf2703abc27226b3dffef3819638483886bb0
Manjunath <manjunath.nm89@gmail.com>
2011-06-03 14:40:36 +0530

    Session-Model

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
+require 'ruby-debug'
 
 describe "Users" do
 
   describe "signup" do
+
     describe "failure" do
-      it "should make a new user" do
-        
+
+      it "should not make a new user" do
+        lambda do
+        debugger
+        visit root_path
+        fill_in "Name",                   :with=>""
+        fill_in "Email",                  :with=>""
+        fill_in "Password",               :with=>""
+        fill_in "Password confirmation",  :with=>""
+        click_button
+        response.should render_template("users")
+        end.should_not change(User,:count)
       end
 
     end
