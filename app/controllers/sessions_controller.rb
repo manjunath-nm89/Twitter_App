1d3eea5955722321c950eb6720262f3c7d736bd1
Manjunath <manjunath.nm89@gmail.com>
2011-06-03 19:34:55 +0530

    Edit Profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @title="Sign In"
     user=User.authenticate(params[:session][:email],
                            params[:session][:password])
     if user.nil?
@@ -18,6 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    @title="Sign out"
     sign_out
     redirect_to root_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ SignupApp::Application.routes.draw do
   resources :users
   resources :sessions, :only => [:new,:create,:destroy]
 
-  match "/welcome", :to => "create#welcome"
-  match "/signin",  :to => "sessions#new"
-  match "/signout", :to => "sessions#destroy"
+  match "/welcome",     :to => "create#welcome"
+  match "/signin",      :to => "sessions#new"
+  match "/signout",     :to => "sessions#destroy"
+  match "/EditProfile", :to => "users#edit"
   
   root :to => "users#new"
 
