8e055014e5da3c76fa124381fced3eaa95752427
Manjunath <manjunath.nm89@gmail.com>
2011-06-07 17:37:43 +0530

    Finished App with social layer

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -10,5 +10,12 @@ class Micropost < ActiveRecord::Base
 
   validates :user_id, :presence => true
 
+  def self.from_users_followed_by(user)
+    #followed_ids = user.following.map(&:id).join(", ")
+    user_ids = user.following.map(&:id)
+    user_ids << user.id
+    #Micropost.where("user_id IN (#{followed_ids}) OR user_id=?",user)
+    Micropost.where(:user_id => user_ids.uniq)
+  end
   
 end
