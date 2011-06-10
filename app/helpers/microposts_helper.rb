6d04fa4c2bdf275559fb6e354a39c3a68b75de74
Manjunath <manjunath.nm89@gmail.com>
2011-06-10 16:32:09 +0530

    Stuck at TextArea Generate

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -8,7 +8,7 @@ module MicropostsHelper
 
   def nested_feed_posts(microposts)
     microposts .map do |message,sub_messages|
-    render(:partial => 'layouts/feeds', :locals => {:f => message} )+content_tag(:div,nested_posts(sub_messages),:class=>"nested_messages")
+    render(:partial => 'layouts/feeds', :locals => {:f => message} )+content_tag(:div,nested_feed_posts(sub_messages),:class=>"nested_messages")
     end.join.html_safe
   end
 
